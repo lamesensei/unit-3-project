dc5f63372f7315acef8fa8313f02816d03719b0d
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-28 18:01:16 +0800

    Update migration for profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_081912) do
+ActiveRecord::Schema.define(version: 2018_10_28_095028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2018_10_28_081912) do
     t.string "username"
     t.string "fullname"
     t.integer "age"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2018_10_28_081912) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
