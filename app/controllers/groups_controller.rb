5411c267047ef41de7f2accc1ed85c6b942d78e5
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-29 11:19:42 +0800

    Add  starter #index/show for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,18 @@
 class GroupsController < ApplicationController
+  def index
+    if params[:code]
+      @group = Group.find_by(code: params[:code])
+      redirect_to group_path(@group)
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:code)
+  end
 end
