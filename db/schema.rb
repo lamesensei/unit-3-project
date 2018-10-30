b8416c15e57c1b9a2ac2b4cc23e21bcedc3ebaaa
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-30 15:00:53 +0800

    Add place to members, add lat/lon to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_080729) do
+ActiveRecord::Schema.define(version: 2018_10_30_065927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_10_29_080729) do
     t.integer "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "lat"
+    t.decimal "lon"
   end
 
   create_table "members", force: :cascade do |t|
@@ -30,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_080729) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "group_id"
+    t.text "place"
     t.index ["group_id"], name: "index_members_on_group_id"
     t.index ["user_id"], name: "index_members_on_user_id"
   end
